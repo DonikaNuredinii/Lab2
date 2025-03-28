7870cbdcb4df791aa97a2d0bb796f5e022df0cec
rronapajaziti <rrona2004@gmail.com>
2025-03-28 23:48:46 +0100

    react and .net config

---
+++ Lab2-Backend/Lab2-Backend/MyContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Lab2_Backend
+{
+    public class MyContext : DbContext
+    {
+        public MyContext(DbContextOptions<MyContext> options) : base(options)
+        {
+        }
+
+        // Example table (DbSet
+    }
+}
